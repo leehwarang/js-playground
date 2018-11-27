02d455fdca663e9f4e20318530b37ec5d00dc1fb
Michelle Lee <hwrng2@gmail.com>
2018-11-27 23:56:43 +0900

    gui_calc 구현중(버튼과 눌렀을 때의 함수 생성)

---
+++ GUI_Calculator/gui_calc(t).js
@@ -0,0 +1,85 @@
+var clickNumbers = function(event){ //객체를 눌렀을 때 객체 자체가 event로 넘어옴
+    console.log("num click");
+    console.log(event.target.innerHTML);
+};
+
+var clickOthers = function(event){
+    console.log("num others");
+    console.log(event.target.innerHTML);
+}
+
+// var input = {};
+
+//         input.init = function(str){
+//             this.list = str.split(" ");
+//             console.log(this.list);
+//         };
+
+//         input.empty = function(){
+//             return this.list.length == 0; //비어 있으면 참, 비어있지 않으면 거짓 
+//         };
+
+//         input.getValue = function(){
+//             var str =  Number(this.list.shift());
+//             return str;
+//         };
+    
+//         input.getOperator = function(){
+//             var op =  this.list.shift();
+                
+//             if (op === '+' || op === '-' || op === '*'
+//                 || op === '/') {
+//                 return op;
+//             }else{
+//                 return "$";
+//             }
+//         };
+
+//         var calculator = {};
+//         calculator.calculate = function(first, second, op){
+//             var ret = first; //메서드 안에서 사용되는 변수(객체의 멤버 변수 x)
+//             switch(op){
+//                 case '+':
+//                 ret += second;
+//                 break;
+
+//                 case '-':
+//                 ret -= second;
+//                 break;
+
+//                 case '*':
+//                 ret *= second;
+//                 break;
+
+//                 case '/':
+//                 ret /= second;
+//                 break;
+
+//                 default: 
+//                 return NaN;
+//             }
+//             return ret;
+//         };
+
+//         var output = {};
+//         output.out = document.getElementById('output');
+
+//         output.print = function(value){
+//             this.out.innerHTML = '최종 결과값은' +value+'입니다.';
+//         };
+
+//         function calc(){
+//             var str = document.getElementById('input').value;
+//             input.init(str);
+//             var result = input.getValue();
+//             console.log(result);
+//             while (!input.empty()){ //비어있지 않을 때 실행, 비어있으면 실행X
+//                 var op = input.getOperator();
+//                 console.log(op);
+//                 var second = input.getValue();
+//                 console.log(second);
+//                 result = calculator.calculate(result, second, op);
+//                 console.log(result);
+//             }
+//             output.print(result);
+//         }
