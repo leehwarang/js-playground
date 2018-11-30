00f1ad6ee2a67be6e64a50aec49afa1d4df9558b
Michelle Lee <hwrng2@gmail.com>
2018-11-30 11:13:04 +0900

    GUI 계산기 혼자 다시 구현해보기 - 완성

--- GUI_Calculator/gui_calc(me).js
+++ GUI_Calculator/gui_calc(me).js
@@ -1,26 +1,114 @@
-var full_str = '';
+var input = {};
+input.array = [];
 
-var clickNumbers = function(event){
-    input_str = event.target.innerHTML;
+input.isEmpty = function(){
+    if (this.array.length === 0){
+        return true;
+    }else{
+        return false;
+    }
+};
 
-    switch(input_str){
-        case 'BS':
-        full_str = full_str.slice(0, -1);
+input.toString = function(){
+    return this.array.join("");
+};
+
+input.preparecal = function(){
+    this.array = this.array.join("").split(" ");
+}
+
+input.getNumber = function(){
+    return this.array.shift()
+};
+
+input.getOperator = function(){
+    return this.array.shift()
+};
+
+input.removeAll = function(result){
+    this.array = [];
+    this.array.push(result);
+}
+
+var output = {};
+output.text = document.getElementById('result');
+
+output.display = function(){
+    //output.text.innerHTML = input.array.join("");
+    output.text.innerHTML = input.toString();
+}
+
+output.printResult = function(result){
+    output.text.innerHTML = result; 
+}
+
+var calculator = {};
+
+
+calculator.startCal = function(first_num, op, second_num){
+    var ret;
+    first_num = Number(first_num);
+    second_num = Number(second_num);
+    
+    switch(op){
+        case '+':
+        ret = first_num + second_num;
         break;
-        case '+': 
-        full_str += ' + ';
+        case '-':
+        ret = first_num - second_num;
         break;
-        case '-': 
-        full_str += ' - ';
+        case '*':
+        ret = first_num * second_num;
         break;
-        case '*': 
-        full_str += ' * ';
-        break;
-        case '/': 
-        full_str += ' / ';
+        case '/':
+        ret = first_num / second_num;
         break;
         default:
-        full_str += input_str
+        alert("계산 할 수 없습니다.");
+    };
+    return ret;
+};
+
+
+var clickNumbers = function(event){
+    str = event.target.innerHTML;
+
+    switch(str){
+        case 'BS':
+        input.array.pop(str);
+        break;
+        case '+': 
+        case '-': 
+        case '*': 
+        case '/': 
+        input.array.push(' '+str+' ');
+        break;
+        default:
+        input.array.push(str);
     }
-    console.log(full_str);
+    if (input.isEmpty()){
+        //입력이 비어 있는 상태
+        output.text.innerHTML = '0';
+    }else{
+        //입력이 있는 상태
+        output.display();
+    }    
+};
+
+var startCalculate = function(){
+    input.preparecal();
+    var result = input.getNumber();
+    console.log(result);
+
+    while(!input.isEmpty()){ //거짓이면 실행. 
+        var op = input.getOperator();
+        console.log(op);
+        var second_num = input.getNumber();
+        console.log(second_num);
+        var result = calculator.startCal(result, op, second_num);
+        console.log(result);
+    }
+    output.printResult(result);
+    input.removeAll(result);
+
 };
