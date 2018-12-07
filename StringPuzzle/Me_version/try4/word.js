cd37f8c866f5cf421fcdef76f61fb4eb1888bb85
Michelle Lee <hwrng2@gmail.com>
2018-12-08 00:31:28 +0900

    15 퍼즐 혼자 다시 구현 중 - 50%의 확률로 단어를 swap시킴 - 단어의 길이의 랜덤값만큼 오른쪽으로 이동시켜 보여지게 함 - 단어와 버튼에 있는 문자열이 같은지 다른지 확인

--- StringPuzzle/Me_version/try4/word.js
+++ StringPuzzle/Me_version/try4/word.js
@@ -1,6 +1,6 @@
 var random_list = 'apple,michelle,frank,programmer,helloworld,ruru,newyork,youonlyliveonce,sanfransico,november'.split(',');
 var base_str = random_list[Math.floor(Math.random()*10)];
-
+var show_result = document.getElementById('result');
 game = {};
 
 game.word_list = base_str.split(""); //단어의 리스트
@@ -23,22 +23,50 @@ game.update = function(){
     }
 }
 
-game.init();
-
+game.check = function(){
+    if (base_str === this.word_list.join('')){
+        show_result.innerHTML = '일치합니다.';
+    }else{
+        show_result.innerHTML = '일치하지 않습니다.';
+    }
+};
 
 var shiftLeft = function(event){
     var s = game.word_list.shift();
     game.word_list.push(s);
     game.update();
+    game.check();
 };
 
 var shiftRight = function(event){
     var s = game.word_list.pop();
     game.word_list.unshift(s);
     game.update();
+    game.check();
 }
 
 var Swap = function(event){
     game.word_list = game.word_list.reverse();
     game.update();
+    game.check();
 }
+
+//단어 길이의 랜덤값만큼 오른쪽으로 이동하여 출력. but 원본 단어를 바꾸지 않기 떄문에 객체 사용 안함.
+var shuffle = function(){ 
+    var n = Math.floor(Math.random() * base_str.length);
+    for (var i = 0; i < n; i++){
+        shiftRight();
+    }
+};
+
+game.init();
+shuffle();
+game.check();
+
+//50%의 확률로 처음에 단어를 swap해서 보여줌
+var toggle = Math.floor(Math.random()*2);
+
+if (toggle){
+    Swap();
+}
+
