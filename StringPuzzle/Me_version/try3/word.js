057c8647684b2bbd37d2157a31f6870ad0119466
Michelle Lee <hwrng2@gmail.com>
2018-12-07 12:58:15 +0900

    15 퍼즐 혼자 처음부터 다시 구현 중 - leftshift, rightshift, swap하고 변경된 문자열을 버튼으로 보여주는 것 리팩토링 - 수정한 부분: base_str을 객체 변수가 아닌, 전역 변수로 변경(처음에 읽어올 때 한 번만 사용하기 때문) - 버튼을 담는 리스트 추가: 버튼에 들어가는 str을 업데이트 할 때마다 버튼을 모두 담고 있는 div 부터 다 불러와야 하는 단점이 있었음. - 업데이트 할 시 리스트를 str으로 재 변경 할 필요 없이 바로 버튼의 innerHTML에 대입할 수 있음.

--- StringPuzzle/Me_version/try3/word.js
+++ StringPuzzle/Me_version/try3/word.js
@@ -1,55 +1,55 @@
+var base_str = document.getElementById('base-str').innerHTML;
+
 game = {};
-game.base_str = document.getElementById('base-str').innerHTML;
-game.base_list = game.base_str.split("");
+
+game.word_list = base_str.split(""); //단어의 리스트
+game.btn_list = []; //버튼의 리스트 
 game.base_btns = document.getElementById('btns-group');
 
-game.shuffle = function(){
-    var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    var alphabet_list = alphabet.split("");
-    var s = '';
-    for (var i = 0; i<5; i++){
-        s += alphabet_list[Math.floor(Math.random()*24)+1];
-    }
-    console.log(s);
-    this.base_str = s;
-};
+// game.shuffle = function(){
+//     var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+//     var alphabet_list = alphabet.split("");
+//     var s = '';
+//     for (var i = 0; i<5; i++){
+//         s += alphabet_list[Math.floor(Math.random()*24)+1];
+//     }
+//     console.log(s);
+//     this.base_str = s;
+// };
 
 game.init = function(){
-    for (var i = 0; i < this.base_str.length; i++){
+    for (var i = 0; i < base_str.length; i++){
         var btn = document.createElement('button');
-        btn.innerHTML = this.base_str[i];
+        btn.innerHTML = base_str[i];
         this.base_btns.appendChild(btn);    
+        this.btn_list.push(btn);
     }
 }
 
 game.update = function(){
-    game.base_str = game.base_list.join("");
 
-    for (var i=0; i<this.base_str.length; i++){
-        this.base_btns.children[i].innerHTML = this.base_str[i];
+    for (var i=0; i<this.word_list.length; i++){
+        this.btn_list[i].innerHTML = this.word_list[i];
     }
 }
 
-game.shuffle();
+// game.shuffle();
 game.init();
 
 
 var shiftLeft = function(event){
-    game.base_list = game.base_str.split("");
-    var s = game.base_list.shift();
-    game.base_list.push(s);
+    var s = game.word_list.shift();
+    game.word_list.push(s);
     game.update();
 };
 
 var shiftRight = function(event){
-    game.base_list = game.base_str.split("");
-    var s = game.base_list.pop();
-    game.base_list.unshift(s);
+    var s = game.word_list.pop();
+    game.word_list.unshift(s);
     game.update();
 }
 
 var Swap = function(event){
-    game.base_list = game.base_str.split("");
-    game.base_list = game.base_list.reverse();
+    game.word_list = game.word_list.reverse();
     game.update();
 }
