3b83e38dae8af2b9d196fffbb1950d53abd145ad
Michelle Lee <hwrng2@gmail.com>
2018-12-10 13:06:48 +0900

    15 퍼즐 강사님 다시 구현 중 - 리팩토링

--- StringPuzzle/T_version/word.js
+++ StringPuzzle/T_version/word.js
@@ -1,81 +1,104 @@
+//html elements
+var word1 = document.getElementById('word1'); //answer
+var word2 = document.getElementById('word2'); //buttons
+var check = document.getElementById('check'); //word1 === word2?
 
-var word2 = document.getElementById('word2')
 
-var words = 'apple,linux,javascript,tutorials,codesquad,baby,girifriend,legend'.split(',');
+//game objects
+var game = {'btns':[]};
+game.words = 'apple,linux,javascript,tutorials,codesquad,baby,girifriend,legend'.split(',');
 
-var word1 = document.getElementById('word1');
-var show = document.getElementById('check');
+//choose 1 word from words
+game.choose = function(){
+    var idx = Math.floor(Math.random()*this.words.length);
+    this.answer = this.words[idx]; //미리 answer객체를 선언하지 않고 바로 선언&할당 동시 가능.
+    this.letters = this.answer.split('');
+    word1.innerHTML = this.answer;
+};
 
-var game = {};
-
-game.choice = function(){
-    var idx = Math.floor(Math.random()*words.length);
-    return words[idx];
-}
-var answer = game.choice(); //이전에는 html에서 지정된 문자열을 가져와 answer로 지정했음
-word1.innerHTML = answer;
-
-game.word = answer.split('');
-game.btns = [];
-game.copy = function(){
-    for (var i = 0; i < this.word.length; i++){
-        this.btns[i].innerHTML = this.word[i];
+game.addButtons = function(){
+    for (var i = 0; i < this.letters.length; i++) {
+        var btn = document.createElement('button');
+        btn.innerHTML = this.letters[i];
+        word2.appendChild(btn);
+        this.btns.push(btn);
     }
 };
 
-game.check = function(){
-    if (answer === this.word.join('')){
-        show.innerHTML = '일치합니다.';
+game.init = function(){
+    this.choose();
+    this.addButtons();
+    this.updateDisplay();
+};
+
+
+game.updateDisplay = function(){
+    var gameStr = this.letters.join('');
+    if (this.answer === gameStr){
+        check.innerHTML = '일치합니다.';
     }else {
-        show.innerHTML = '일치하지 않습니다.';
+        check.innerHTML = '일치하지 않습니다.';
     }
 };
 
-
-for (var i = 0; i < answer.length; i++) {
-    var btn = document.createElement('button');
-    btn.innerHTML = answer[i];
-    word2.appendChild(btn);
-    game.btns.push(btn);
-}
-
-var swap = function (event) {
-    game.word = game.word.reverse();
-
-    game.copy();
-    game.check();
-    
+game.copyBtnText = function(){
+    for (var i = 0; i < this.letters.length; i++){
+        this.btns[i].innerHTML = this.letters[i];
+    }
 };
 
-var rshift = function (event) {
-    var s = game.word.pop();
-    game.word.unshift(s);
-    
-    game.copy();
-    game.check();
+//event handler for swap button
+var swap = function () {
+    var temp = [];
+    //copy and swap
+    while (game.letters.length != 0){
+        var s = game.letters.pop();
+        temp.push(s);
+    }
+
+    game.letters = temp;
+    game.copyBtnText();
+    game.updateDisplay();
+
+    //원래 코드
+    // game.word = game.word.reverse();
+    // game.copy();
+    // game.check();
 };
 
-var lshift = function (event) {
+var rshift = function () {
+    var s = game.letters.pop();
+    game.letters.unshift(s);
+
+    game.copyBtnText();
+    game.updateDisplay();
+};
+
+var lshift = function () {
     console.log('lshift');
     var s = game.word.shift();
     game.word.push(s);
 
-    game.copy();
-    game.check();
+    game.copyBtnText();
+    game.updateDisplay();
 };
 
 //shuffle
+game.shuffle = function(){
+    var toggle = Math.floor(Math.random()*2) === 0;
 
-var toggle = Math.floor(Math.random()*2) === 0;
+    if (toggle){
+        swap();
+    }
+    
+    var n = Math.floor(Math.random() * game.answer.length);
+    
+    for (var i=0; i<n; i++){
+        rshift();
+    }
+};
 
-if (toggle){
-    swap();
-}
-
-var n = Math.floor(Math.random() * answer.length);
-
-for (var i=0; i<n; i++){
-    rshift();
-}
-
-game.check();
+//코드의 실행부
+game.init(); //이전에는 html에서 지정된 문자열을 가져와 answer로 지정했음
+game.shuffle();
+game.updateDisplay();
