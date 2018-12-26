717d40b1ec54626f6b9e59efe12e8a866e3434b2
Michelle Lee <hwrng2@gmail.com>
2018-12-26 13:38:07 +0900

    GUI 계산기 혼자 재구현중: 기능 추가 - 이벤트 위임 방식으로 js 파일 연결 - 버튼 클릭했을 때마다 리스트에 버튼 추가

--- codesquad_level1/Re_GUI_Calculator/gui_cal.js
+++ codesquad_level1/Re_GUI_Calculator/gui_cal.js
@@ -1,5 +1,28 @@
-var btns = document.getElementsByClassName('btns');
+var input = {};
+input.btn_list = [];
 
-btns.addEventListener('click', function(){
-    console.log('hello')
-});
+input.init = function(){
+    input.btn_list = [];
+}
+
+input.add_btn = function (e){
+   //this.btn_list.push()
+   if (e.target.className == 'btns-num'){
+       this.btn_list.push(e.target.innerHTML);
+       console.log(this.btn_list);
+   }else if(e.target.className == 'btns-operator'){
+       this.btn_list.push(' '+e.target.innerHTML+' ');
+       console.log(this.btn_list);
+   }
+}
+
+
+document.addEventListener('DOMContentLoaded', input.init());
+//이벤트 위임 방식으로 구현
+var btns_group = document.getElementById('btns-group').addEventListener('click', function(e){
+    // console.log('[taget]: '+ e.target);
+    // console.log('[target.nodeName]: ' + e.target.nodeName);
+    if (e.target && e.target.nodeName == 'BUTTON'){
+        input.add_btn(e);
+    };
+})
