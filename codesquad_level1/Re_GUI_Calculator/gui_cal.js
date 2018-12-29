6b74a36dac9f3a854b31a14ac96bb5afca4974b4
Michelle Lee <hwrng2@gmail.com>
2018-12-29 14:48:53 +0900

    GUI 계산기 혼자 재구현중: 기능 추가 - 클릭된 버튼 리스트를 문자열로 변환하여 display에 식을 보여주는 것까지 구현

--- codesquad_level1/Re_GUI_Calculator/gui_cal.js
+++ codesquad_level1/Re_GUI_Calculator/gui_cal.js
@@ -1,27 +1,32 @@
+var display = document.getElementById('display-board');
 var input = {};
 input.btn_list = [];
+input.btn_str = '';
+
 
 input.init = function(){
     input.btn_list = [];
 }
 
+input.displayUpdate = function(){
+    this.btn_str = this.btn_list.join("");
+    display.innerHTML = this.btn_str;
+};
+
 input.add_btn = function (e){
-   //this.btn_list.push()
    if (e.target.className == 'btns-num'){
        this.btn_list.push(e.target.innerHTML);
-       console.log(this.btn_list);
+       this.displayUpdate();
    }else if(e.target.className == 'btns-operator'){
        this.btn_list.push(' '+e.target.innerHTML+' ');
-       console.log(this.btn_list);
+       this.displayUpdate();
    }
 }
 
-
 document.addEventListener('DOMContentLoaded', input.init());
+
 //이벤트 위임 방식으로 구현
 var btns_group = document.getElementById('btns-group').addEventListener('click', function(e){
-    // console.log('[taget]: '+ e.target);
-    // console.log('[target.nodeName]: ' + e.target.nodeName);
     if (e.target && e.target.nodeName == 'BUTTON'){
         input.add_btn(e);
     };
