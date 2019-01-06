26a18d6137c9087986ea1487beb484414c1645f2
Michelle Lee <hwrng2@gmail.com>
2019-01-06 22:05:24 +0900

    로또 추첨기 1차 구현 완료

---
+++ project/lotto/lotto.js
@@ -0,0 +1,86 @@
+
+//초기화 코드 추가 필요.
+
+var btns = document.getElementsByClassName('btn-num');
+var user = {
+    choice_num : []
+};
+
+var lotto = {
+    random_btn : [],
+    random_num : [],
+    bonus_num : 0, 
+    cnt : 0, 
+    bonusCompare : function(){
+        for (var k=0; k<this.random_num.length; k++){
+            if (this.bonus_num === this.random_num[k]){
+                return true
+            }
+        }
+    },
+    startCompare : function(){
+        cnt = 0;
+        bonus_num = this.random_num.pop();
+        for (var i=0; i<this.random_num.length; i++){
+            for (var j=0; j<user.choice_num.length; j++){
+                if (this.random_num[i] == user.choice_num[j]){
+                    cnt += 1
+                }
+            }
+        }
+        return cnt
+    },
+    showResult : function(){
+        if (this.cnt === 6){
+            alert("축하합니다! 1등이에요!");
+        }else if (this.cnt === 5){
+            if(bonusCompare() === true){ 
+                alert("축하합니다! 2등이에요!");
+            }else{
+                alert("축하합니다! 3등이에요!");
+            }
+        }else if (this.cnt === 4){
+            alert("축하합니다! 4등이에요!");
+        }else if (this.cnt === 3){
+            alert("축하합니다! 4등이에요!");
+        }else{
+            alert("꽝!");
+        }
+    },
+    random_choice : function(){
+        for (var i = 0; i <7; i++){
+            var n = Math.floor(Math.random() * btns.length + 1);
+            this.random_btn.push(btns[n]); //뽑았는데 중복되는 경우 없애는 코드 추가 필요. 
+            
+            if (i == 6){
+                this.random_btn[i].style.borderColor = '#FFD740';
+            }else{
+                this.random_btn[i].style.borderColor = 'red';
+            }
+        }
+
+        for (var el of this.random_btn){
+            // console.log(el);
+            this.random_num.push(el.innerHTML);
+        }
+        console.log("로또에서 추첨한 숫자:" + this.random_num);
+        
+        var correct_cnt = this.startCompare();
+        console.log("맞춘 개수: " + correct_cnt)
+        this.showResult();
+        
+    }
+};
+
+
+//숫자 버튼에 클릭했을 때 이벤트를 저장하는 코드
+document.getElementById('btns-num').addEventListener('click', function(event){
+    user.choice_num.push(event.target.innerHTML);
+    event.target.style.backgroundColor = '#2196F3';
+    if (user.choice_num.length === 6){
+        alert("모든 숫자를 다 입력하셨습니다.");
+        console.log("사용자가 입력한 숫자:" + user.choice_num);
+        lotto.random_choice();
+    }
+});
+
