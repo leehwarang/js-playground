2bc6e4837a5af66dff645b121e4dc776171d5a68
Michelle Lee <hwrng2@gmail.com>
2019-01-17 16:22:59 +0900

    todo app 구현중 - 체크하면 취소선으로 완료 표시, 다시 체크하면 해제 표시 완료

--- project/todo/todo.js
+++ project/todo/todo.js
@@ -4,37 +4,52 @@ var todo_list = document.getElementById('todo-list');
 
 var i = 1;
 
+function onClickHandler(e){
+    console.log(e.target.checked);
+    var checked_id = e.target.id;
+    var label_list = document.getElementsByTagName('label');
+    for (var i=0; i<label_list.length; i++){
+        if (e.target.checked == true){
+            if (label_list[i].getAttribute('for') === checked_id){
+                label_list[i].style.textDecoration = "line-through";
+            }
+        }else{
+            if (label_list[i].getAttribute('for') === checked_id){
+                label_list[i].style.textDecoration = 'none';
+            }
+        }
+    }
+}
+
 
 input.addEventListener("keyup", function(event){
     if (event.keyCode === 13){
         if (input.value !== ''){
             var chk = document.createElement('input');   
             chk.setAttribute('type', 'checkbox');
-            // chk.setAttribute('value', input.value);
             chk.setAttribute('id', 'thing'+i);
             chk.setAttribute('name', 'todothing');
+            chk.setAttribute('onclick', 'onClickHandler(event)');
 
             var lbl = document.createElement('label')
             lbl.setAttribute('for', 'thing'+i)
 
             lbl.appendChild(document.createTextNode(input.value));
 
-
             todo_list.appendChild(chk);
             todo_list.appendChild(lbl);
 
             input.value = '';
             i += 1
-
         }
     }
         
 });
 
 
-document.addEventListener('DOMContentLoaded', function(){
-    console.log("test");
-});
+// document.addEventListener('DOMContentLoaded', function(){
+//     console.log("test");
+// });
 
 //엔터를 치면 해야할 일
 //1. 입력 받은 것을 list의 항목으로 보내야함
