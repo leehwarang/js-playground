36e0e64c64005b9e75c35e9e0037a86866905ca7
Michelle Lee <hwrng2@gmail.com>
2019-02-17 23:42:20 +0900

    야구 게임 만드는 중

---
+++ project/numberbaseball/number_baseball.js
@@ -0,0 +1,46 @@
+var computer = {
+  number: ""
+};
+var player = {
+  number: ""
+};
+
+var game = {
+  strike: 0,
+  ball: 0,
+  nothing: 0
+};
+
+var choice = function() {
+  for (var i = 0; i < 3; i += 1) {
+    computer.number += String(Math.floor(Math.random() * 9) + 1);
+  }
+  console.log(computer.number);
+};
+
+var gamestart = function() {
+  for (var i in player.number) {
+    if (
+      computer.number.includes(player.number[i]) &&
+      i == computer.number.indexOf(player.number[i])
+    ) {
+      game.strike += 1;
+      computer.number = computer.number.replace(player.number[i], "0");
+    } else if (computer.number.includes(player.number[i])) {
+      game.ball += 1;
+    } else {
+      game.nothing += 1;
+    }
+  }
+};
+
+var resultshow = function() {
+  console.log(
+    game.strike + "스트라이크, " + game.ball + "볼, " + game.nothing + "낫싱"
+  );
+};
+
+choice();
+player.number = prompt("숫자를 입력하세요. : ");
+gamestart();
+resultshow();
