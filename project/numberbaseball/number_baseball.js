f982710d7c5b81fae981472b42c1306807f20b54
Michelle Lee <hwrng2@gmail.com>
2019-02-21 01:07:56 +0900

    숫자 야구 더 간단히 개선

--- project/numberbaseball/number_baseball.js
+++ project/numberbaseball/number_baseball.js
@@ -15,23 +15,32 @@ var choice = function() {
   for (var i = 0; i < 3; i += 1) {
     computer.number += String(Math.floor(Math.random() * 9) + 1);
   }
-  console.log(computer.number);
+  console.log("컴퓨터가 뽑은 값: " + computer.number);
 };
 
 var gamestart = function() {
-  for (var i in player.number) {
-    if (
-      computer.number.includes(player.number[i]) &&
-      i == computer.number.indexOf(player.number[i])
-    ) {
+  for (var i = 0; i < 3; i += 1) {
+    if (computer.number[i] === player.number[i]) {
       game.strike += 1;
-      computer.number = computer.number.replace(player.number[i], "0");
     } else if (computer.number.includes(player.number[i])) {
       game.ball += 1;
     } else {
       game.nothing += 1;
     }
   }
+  // for (var i in player.number) {
+  //   if (
+  //     computer.number.includes(player.number[i]) &&
+  //     i == computer.number.indexOf(player.number[i])
+  //   ) {
+  //     game.strike += 1;
+  //     computer.number = computer.number.replace(player.number[i], "0");
+  //   } else if (computer.number.includes(player.number[i])) {
+  //     game.ball += 1;
+  //   } else {
+  //     game.nothing += 1;
+  //   }
+  // }
 };
 
 var resultshow = function() {
@@ -42,5 +51,6 @@ var resultshow = function() {
 
 choice();
 player.number = prompt("숫자를 입력하세요. : ");
+console.log("사용자가 입력한 값: " + player.number);
 gamestart();
 resultshow();
